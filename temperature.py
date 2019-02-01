9bc4f65c651317cf2d5e8d542962e2ecd352e272
nicyann <43368192+nicyann@users.noreply.github.com>
2019-02-01 11:03:47 +0100

    update temperature

--- temperature.py
+++ temperature.py
@@ -1,39 +1,14 @@
-# Imports
-import os
-import glob
 import time
+from temperatureSensor import TemperatureSensor
+capteur = TemperatureSensor('28-021313bf0eaa')
 
-# Intialisation des broches
-os.system('modprobe w1-gpio')  # Allume le module 1wire
-os.system('modprobe w1-therm')  # Allume le module Temperature
 
-# Chemin du fichier contenant la température (remplacer par votre valeur trouvée précédemment)
-device_file = '/sys/bus/w1/devices/28-021313bf0eaa/w1_slave'
-
-# Une fonction qui lit dans le fichier température
-def read_temp_raw():
-    f = open(device_file, 'r')  # Ouvre le dichier
-    lines = f.readlines() # Returns the text
-    f.close()
-    return lines
-    
-# Lis la temperature 
-def read_temp():
-    lines = read_temp_raw()  # Lit le fichier de température
-    # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
-    # On relis ensuite le fichier
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-	# On cherche le '=' dans la seconde ligne du fichier
-    equals_pos = lines[1].find('t=')
-    # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
-        
-# On affiche la temérature tant que le script tourne
-while True:
-    print(read_temp())
-    time.sleep(1)
+type_temp = input('Quelle unite de température voulez-vous chosir (F ou C) : ')
+if 'C':
+    while True:
+        print(capteur.read_temp())
+        time.sleep(1)
+elif 'F':
+    while True:
+        print(capteur.read_temp_far())
+        time.sleep(1)
